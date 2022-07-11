dc931a564f37f2a942520484dea6e50f5474d95d
trevor-arthur <trevor.spaniola@protonmail.com>
2022-07-10 20:48:19 -0700

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"shodan/shodan"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load in API key from .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("[!] Error loading .env file")
+	}
+	apiKey := os.Getenv("SHODAN_API_KEY")
+
+	// Anything longer than 2 words ('shodan <searchterm>') will return usage help
+	if len(os.Args) != 2 {
+		log.Fatalln("[*] Usage: shodan <searchterm>")
+	}
+
+	s := shodan.New(apiKey)
+	info, err := s.APIInfo()
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Printf(
+		"Query Credits: %d\nScan Credits: %d\n\n",
+		info.QueryCredits,
+		info.ScanCredits,
+	)
+
+	hostSearch, err := s.HostSearch(os.Args[1])
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, host := range hostSearch.Matches {
+		fmt.Printf("%18s%8d\n", host.IPString, host.Port)
+	}
+}
---
+++ shodan/api.go
@@ -0,0 +1,33 @@
+package shodan
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type APIInfo struct {
+	ScanCredits  int      `json:"scan_credits"`
+	UsageLimits  []string `json:"usage_limits"`
+	Plan         string   `json:"plan"`
+	HTTPS        bool     `json:"https"`
+	Unlocked     bool     `json:"unlocked"`
+	QueryCredits int      `json:"query_credits"`
+	MonitoredIPs int      `json:"monitored_ips"`
+	UnlockedLeft int      `json:"unlocked_left"`
+	Telnet       bool     `json:"telnet"`
+}
+
+func (s *Client) APIInfo() (*APIInfo, error) {
+	res, err := http.Get(fmt.Sprintf("%s/api-info?key=%s", BaseURL, s.apiKey))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var ret APIInfo
+	if err := json.NewDecoder(res.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
